aca125f6f4045ea91d76fe83342121bda5f1605d
sebastian <sebastian.a.donoso.b@gmail.com>
2019-06-27 15:47:30 -0400

    test models

--- sleepBot/models.py
+++ sleepBot/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=150)
+    id_telegram = models.CharField(max_length=150)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+
+class Data(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True, blank=True)
+    sleep_hours = models.IntegerField()
+    mood = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    time_stamp = models.DateTimeField(default=timezone.now)
--- sleepBot/tests.py
+++ sleepBot/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from datetime import datetime
+from .models import Person, Data
+from django.utils import timezone
 
-# Create your tests here.
+
+# Create your tests here
+class SleepModelTest(TestCase):
+
+    def setUp(self):
+        person = Person.objects.create(id_telegram="juanito", name="juan")
+        Data.objects.create(person=person, sleep_hours=8, mood=4)
+        Data.objects.create(person=person, sleep_hours=10, mood=11)
+
+    def test_get_person(self):
+        person = Person.objects.get(id_telegram="juanito")
+        self.assertEquals(person.name, "juan")
+        self.assertEquals(person.creation_date.day, 27)
+
+    def test_data(self):
+        person = Person.objects.get(id_telegram="juanito")
+        data = Data.objects.filter(person=person)
+        self.assertEquals(len(data),2)
+        self.assertEquals(data[0].mood,4)
+        self.assertEquals(data[1].mood,11)
