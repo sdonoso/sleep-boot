d4dbb6f1f96f3934c625c9f997fe3c228fd88d78
sebastian <sebastian.a.donoso.b@gmail.com>
2019-07-01 16:03:32 -0400

    new sleep bot basic

---
+++ script-bot/messages.py
@@ -0,0 +1,3 @@
+START_TEXT = "Hola mi nombre es sleepBot y estoy para ayudarte a mejorar tu calidad de sueño y tu ánimo, lo siguiente que debes hacer es enviarme tu nombre."
+
+REGISTRATION_TEXT = "Muchas gracias por registrarte. Próximamente te estaremos pidiendo cuántas horas has dormido y tu ánimo durante el día. Al final de la semana, te mostraremos una estadística de tu sueño y ánimo en los últimos 7 días."
