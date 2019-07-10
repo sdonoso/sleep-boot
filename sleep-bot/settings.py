fbf983a748610c0124456b3243a758b4ffb45147
sebastian <sebastian.a.donoso.b@gmail.com>
2019-07-10 13:55:51 -0400

    add statuc

--- sleep-bot/settings.py
+++ sleep-bot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!r$ai+2z69+&psfey3h2lk#-45-b7%1+oefbbs(svyt9q-ed=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', "localhost"]
 
